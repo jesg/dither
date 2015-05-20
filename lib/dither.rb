6e710fe323605495963e2d79565e73307b3145d1
Jason Gowan <gowanjason@gmail.com>
2015-05-20 17:11:31 -0500

    add mipog

--- lib/dither.rb
+++ lib/dither.rb
@@ -6,6 +6,10 @@ module Dither
   def self.all_pairs(params, t = 2, opts = {})
     IPOG.new(params, t, opts).run
   end
+
+  def self.mipog(params, t = 2, opts = {})
+    MIPOG.new(params, t, opts).run
+  end
 end # Dither
 
 require 'dither/param'
@@ -13,3 +17,4 @@ require 'dither/unbound_param'
 require 'dither/test_case'
 require 'dither/ipog_helper'
 require 'dither/ipog'
+require 'dither/mipog'
